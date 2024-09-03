5b0fd54180ebf543d3c917dcfe7484ca715c3b6e
Rehanhubro1 <xlrzishan@gmail.com>
2024-09-03 23:56:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7301235031:AAHQ4O0MYQ9644US-Rof2i8O8aYco_bInI0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("alokhubro")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6947543779")
 
 
 
