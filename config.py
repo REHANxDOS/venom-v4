3294ed0fa27553c6d723428b75a6af73b4047f25
REHANxDOS <khatriikram659@gmail.com>
2024-09-03 23:47:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY Ronit
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7318307196:AAHRZmtcaTP4W926cMvszDYVl_3lsR0vtHI")
+BOT_TOKEN=("6376938455:AAF0O6Je9X5Lxwa5GG7kJ_CaeV-4nZ7esQI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("RONIT_IN")
+OWNER_USERNAME=("REHANxCRAZY")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("7246521618")
+ADMIN_IDS=("5239197123")
 
 
 
