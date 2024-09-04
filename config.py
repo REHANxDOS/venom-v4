71cebdf5a0bb298839c44648456b634ec52fe48e
REHANxDOS <khatriikram659@gmail.com>
2024-09-04 12:52:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7301235031:AAHQ4O0MYQ9644US-Rof2i8O8aYco_bInI0")
+BOT_TOKEN=("6376938455:AAF0O6Je9X5Lxwa5GG7kJ_CaeV-4nZ7esQI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("alokhubro")
+OWNER_USERNAME=("REHANxCRAZY")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6947543779")
+ADMIN_IDS=("5239197123")
 
 
 
