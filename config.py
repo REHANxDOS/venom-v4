76d0926eac09993d907ff72ebf622679800b620f
REHANxDOS <khatriikram659@gmail.com>
2024-09-04 12:55:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("6376938455:AAF0O6Je9X5Lxwa5GG7kJ_CaeV-4nZ7esQI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("REHANxCRAZY")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5239197123")
 
 
 
